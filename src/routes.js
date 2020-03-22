c875a241f265a294a5f54e7bd6658fb16b2ce50a
LucasFelinto <elucasfelinto@gmail.com>
2020-03-22 12:15:24 -0300

    :wrench: criando arquivo de rotas

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Main from './pages/Main';
+import Repositroy from './pages/Repository';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route path="/repostiroy" component={Repositroy} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
